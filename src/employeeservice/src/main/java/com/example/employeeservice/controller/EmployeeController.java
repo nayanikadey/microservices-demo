581fe530eab2aff59b8e9271be20e8b3595a42c3
Nayanika Dey <nayanikd@thoughtworks.com>
2023-02-04 10:10:29 +0530

    Add custom metrics counter for department search

--- src/employeeservice/src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/employeeservice/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.repository.EmployeeRepository;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,14 @@ public class EmployeeController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 
+    private final Counter departmentSearchCounter;
+
+    public EmployeeController(MeterRegistry registry) {
+        departmentSearchCounter = registry.counter("departmentSearchCounter");
+    }
     @Autowired
     EmployeeRepository repository;
+
 
     @PostMapping("/")
     public Employee add(@RequestBody Employee employee) {
@@ -42,6 +50,7 @@ public class EmployeeController {
     @GetMapping("/department/{departmentId}")
     public List<Employee> findByDepartment(@PathVariable("departmentId") String departmentId) {
         LOGGER.info("Employee find: departmentId={}", departmentId);
+        departmentSearchCounter.increment();
         return repository.findByDepartmentId(departmentId);
     }
 
